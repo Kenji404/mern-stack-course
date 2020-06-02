d319baa771522d982cf98ccba0122710946f1bce
Kenji404 <kenji.andre@gmail.com>
2020-06-01 22:38:46 -0300

    commit 2

---
+++ 01-aula/servidor/server.js
@@ -0,0 +1,14 @@
+const http = require('http');
+const requisicao = function(_, response) {
+    response.writeHead(200, { "Content-Type": "text/html" });
+    response.write("<h1>Texto a ser exibido</h1>");
+    response.end();
+}
+
+const server = http.createServer(requisicao);
+
+const servidorNoAr = function() {
+    console.log("servidor no ar!");
+}
+
+server.listen(3000, servidorNoAr);
